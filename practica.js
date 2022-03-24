4ec14d4f04a717a0d71408312361a9a88a1c129a
Josue Alfonzo <16-00436@usb.ve>
2022-03-24 11:13:18 -0400

    Primer commit de mi proyecto

---
+++ practica.js
@@ -0,0 +1,10 @@
+
+
+const miFuncion = ( name , lastName, nickname ) => {
+    let fullName = `${name} ${lastName}`;
+    return `Mi nombre es ${fullName}, pero prefiero qe me digas ${nickname} `;
+}
+
+console.log (miFuncion('Josue Gerardo', 'Alfonzo Angulo', 'Gocho'));
+
+
